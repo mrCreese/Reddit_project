b5c83f1346f72715737191ef5cf229548fc463ac
mrCreese <99073893+mrCreese@users.noreply.github.com>
2023-01-26 17:00:27 +0100

    eliminate old date_helper function

--- src/app/helpers/date_helpers.jsx
+++ src/app/helpers/date_helpers.jsx
@@ -1,6 +1,4 @@
 export function calculateData(timestamp) {
-    /*    let data = (new Date(timestamp * 1000) + '').split(' ');
-    return `${data[2]}.${data[1]}.${data[3]} ${data[4]}`; */
     const data = new Date(timestamp * 1000);
     return data.toLocaleString();
 }
